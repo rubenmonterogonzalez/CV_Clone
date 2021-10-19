b5ca3302b7e63cda0d82dd630c60970230572b7b
rubenmonterogonzalez <rubenmontero@rocketmail.com>
2021-10-19 17:15:31 +0200

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+"use sctrict";
+
+document.querySelector(".menu_icon").addEventListener("click", showMobileNav);
+
+function showMobileNav() {
+  document.querySelector(".menu_icon").innerHTML = "Hello World";
+
+}
